2a2341156657414c8e2dcce750db6872a488aa59
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2026-02-03 15:46:33 +0300

    Create components

---
+++ src/components/Player/Player.ts
@@ -0,0 +1,18 @@
+import * as THREE from "three";
+
+class Player {
+  body: THREE.Mesh;
+  constructor() {
+    this.body = new THREE.Mesh(
+      new THREE.BoxGeometry(15, 15, 20),
+      new THREE.MeshLambertMaterial({
+        color: "white",
+        flatShading: true,
+      }),
+    );
+
+    this.body.position.z = 10;
+  }
+}
+
+export const player = new Player();
