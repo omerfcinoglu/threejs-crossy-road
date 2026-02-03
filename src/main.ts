cb0fa6ea3cb13802f5346ea3e2fb97077167fa6c
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2026-02-03 15:46:45 +0300

    Render Player with Lights

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,18 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import * as THREE from "three";
+import { Camera } from "./components/Camera";
+import { Renderer } from "./components/Renderer";
+import { player } from "./components/Player/Player";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const ambientLight = new THREE.AmbientLight();
+const dirLight = new THREE.DirectionalLight();
+dirLight.position.set(-100, -100, -100);
+scene.add(ambientLight);
+scene.add(dirLight);
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const camera = Camera();
+scene.add(camera);
+scene.add(player.body);
+
+const renderer = Renderer();
+renderer.render(scene, camera);
