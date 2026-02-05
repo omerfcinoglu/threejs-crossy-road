eebae93f7348834fe96f4c4a1d00ad81ef439bbf
Omer Faruk Cinoglu <62906173+omerfcinoglu@users.noreply.github.com>
2026-02-05 18:53:18 +0300

    Update Data.ts

--- src/Data.ts
+++ src/Data.ts
@@ -1,4 +1,6 @@
-export const metaData = [
+import type { RowData } from "./types";
+
+export const metaData: RowData[] = [
   {
     type: "forest",
     trees: [
@@ -13,9 +15,9 @@ export const metaData = [
     direction: true,
     speed: 3,
     vehicles: [
-      { initialTileIndex: -3, color: 0x3498db },
-      { initialTileIndex: 1, color: 0xe74c3c },
-      { initialTileIndex: 5, color: 0xf1c40f },
+      { initialTileIndex: -3, color: 0x3498db, ref: undefined },
+      { initialTileIndex: 1, color: 0xe74c3c, ref: undefined },
+      { initialTileIndex: 5, color: 0xf1c40f, ref: undefined },
     ],
   },
   {
@@ -31,8 +33,8 @@ export const metaData = [
     direction: false,
     speed: 2,
     vehicles: [
-      { initialTileIndex: -4, color: 0x2ecc71 },
-      { initialTileIndex: 2, color: 0x9b59b6 },
+      { initialTileIndex: -4, color: 0x2ecc71, ref: undefined },
+      { initialTileIndex: 2, color: 0x9b59b6, ref: undefined },
     ],
   },
   {
@@ -40,8 +42,8 @@ export const metaData = [
     direction: false,
     speed: 4,
     vehicles: [
-      { initialTileIndex: 0, color: 0x1abc9c },
-      { initialTileIndex: 4, color: 0xff8800 },
+      { initialTileIndex: 0, color: 0x1abc9c, ref: undefined },
+      { initialTileIndex: 4, color: 0xff8800, ref: undefined },
     ],
   },
   {
